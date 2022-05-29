4a64ca7c3d85d9424f79698dcf8e2fc0c032f771
Baljibeka <87861742+Baljibeka@users.noreply.github.com>
2022-05-29 15:30:48 +0600

    first commit

---
+++ TestCases/First Test Case/mrzRecognition/recognizer.py
@@ -0,0 +1,165 @@
+'''
+    * Copyright (C) 2011-2020 Doubango Telecom <https://www.doubango.org>
+    * File author: Mamadou DIOP (Doubango Telecom, France).
+    * License: For non commercial use only.
+    * Source code: https://github.com/DoubangoTelecom/ultimateMRZ-SDK
+    * WebSite: https://www.doubango.org/webapps/mrz/
+    https://github.com/DoubangoTelecom/ultimateMRZ/blob/master/SDK_dist/samples/c++/recognizer/README.md
+	Usage: 
+		recognizer.py \
+			--image <path-to-image-with-mrzdata-to-recognize> \
+			[--assets <path-to-assets-folder>] \
+            [--backprop <whether-to-enable-backpropagation:true/false>] \
+			[--vcheck <whether-to-enable-vertical-check:true/false>] \
+            [--ielcd <whether-to-enable-IELCD:true/false>] \
+			[--tokenfile <path-to-license-token-file>] \
+			[--tokendata <base64-license-token-data>]
+	Example:
+		recognizer.py \
+			--image C:/Projects/GitHub/ultimate/ultimateMRZ/SDK_dist/assets/images/Czech_passport_2005_MRZ_orient1_1300x1002.jpg \
+			--assets C:/Projects/GitHub/ultimate/ultimateMRZ/SDK_dist/assets \
+            --backprop True \
+			--vcheck True \
+			--tokenfile C:/Projects/GitHub/ultimate/ultimateMRZ/SDK_dev/tokens/windows-iMac.lic
+'''
+
+import ultimateMrzSdk
+import sys
+import argparse
+import json
+import platform
+import os.path
+
+TAG = "[PythonRecognizer] "
+
+# Defines the default JSON configuration. More information at https://www.doubango.org/SDKs/mrz/docs/Configuration_options.html
+JSON_CONFIG = {
+    "debug_level": "info",
+    "debug_write_input_image_enabled": False,
+    "debug_internal_data_path": ".",
+        
+    "num_threads": -1,
+    "gpgpu_enabled": True,
+    "gpgpu_workload_balancing_enabled": False,
+        
+    "segmenter_accuracy": "high",
+    "gamma": -1,
+    "interpolation": "bilinear",
+    "min_num_lines": 2,
+    "roi": [0, 0, 0, 0],
+    "min_score": 0.0
+}
+
+IMAGE_TYPES_MAPPING = { 
+        'RGB': ultimateMrzSdk.ULTMRZ_SDK_IMAGE_TYPE_RGB24,
+        'RGBA': ultimateMrzSdk.ULTMRZ_SDK_IMAGE_TYPE_RGBA32,
+        'L': ultimateMrzSdk.ULTMRZ_SDK_IMAGE_TYPE_Y
+}
+
+# Load image
+def load_pil_image(path):
+    from PIL import Image, ExifTags, ImageOps
+    import traceback
+    pil_image = Image.open(path)
+    img_exif = pil_image.getexif()
+    ret = {}
+    orientation  = 1
+    try:
+        if img_exif:
+            for tag, value in img_exif.items():
+                decoded = ExifTags.TAGS.get(tag, tag)
+                ret[decoded] = value
+            orientation  = ret["Orientation"]
+    except Exception as e:
+        print(TAG + "An exception occurred: {}".format(e))
+        traceback.print_exc()
+
+    if orientation > 1:
+        pil_image = ImageOps.exif_transpose(pil_image)
+
+    if pil_image.mode in IMAGE_TYPES_MAPPING:
+        imageType = IMAGE_TYPES_MAPPING[pil_image.mode]
+    else:
+        raise ValueError(TAG + "Invalid mode: %s" % pil_image.mode)
+
+    return pil_image, imageType
+
+# Check result
+def checkResult(operation, result):
+    if not result.isOK():
+        print(TAG + operation + ": failed -> " + result.phrase())
+        assert False
+    else:
+        print(TAG + operation + ": OK -> " + result.json())
+
+# Entry point
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""
+    This is the recognizer sample using python language
+    """)
+
+    parser.add_argument("--image", required=True, help="Path to the image with MRZ data to recognize")
+    parser.add_argument("--assets", required=False, default="../../../assets", help="Path to the assets folder")
+    parser.add_argument("--backprop", required=False, default=platform.processor()=='i386', help="Whether to enable backpropagation to detect the MRZ lines. Technical description at https://www.doubango.org/SDKs/mrz/docs/Detection_techniques.html#backpropagation. Default: true for x86 CPUs and false for ARM CPUs.")
+    parser.add_argument("--vcheck", required=False, default=platform.processor()=='i386', help="Whether to enable vertical check to detect +/-90deg rotated images. Default: true for x86 CPUs and false for ARM CPUs.")
+    parser.add_argument("--ielcd", required=False, default=platform.processor()=='i386', help="Whether to enable Image Enhancement for Low Contrast Document (IELCD). More information at https://www.doubango.org/SDKs/mrz/docs/IELCD.html. Default: true for x86 CPUs and false for ARM CPUs.")    
+    parser.add_argument("--tokenfile", required=False, default="", help="Path to license token file")
+    parser.add_argument("--tokendata", required=False, default="", help="Base64 license token data")
+
+    args = parser.parse_args()
+    IMAGE = args.image
+    ASSETS = args.assets
+    TOKEN_FILE = args.tokenfile
+    TOKEN_DATA = args.tokendata
+    
+
+    # Check if image exist
+    if not os.path.isfile(IMAGE):
+        raise OSError(TAG + "File doesn't exist: %s" % IMAGE)
+
+    # Decode the image and extract type
+    image, imageType = load_pil_image(IMAGE)
+    width, height = image.size
+
+    # Update JSON options using values from the command args
+    if ASSETS:
+        JSON_CONFIG["assets_folder"] = ASSETS
+    if TOKEN_FILE:
+        JSON_CONFIG["license_token_file"] = TOKEN_FILE
+    if TOKEN_DATA:
+        JSON_CONFIG["license_token_data"] = TOKEN_DATA
+
+    JSON_CONFIG["backpropagation_enabled"] = (args.backprop == "True")
+    JSON_CONFIG["vertical_check_enabled"] = (args.vcheck == "True")
+    JSON_CONFIG["ielcd"] = (args.ielcd == "True")
+
+
+    # Initialize the engine
+    checkResult("Init", 
+                ultimateMrzSdk.UltMrzSdkEngine_init(json.dumps(JSON_CONFIG))
+               )
+
+    # Recognize/Process
+    # Please note that the first time you call this function all deep learning models will be loaded 
+    # and initialized which means it will be slow. In your application you've to initialize the engine
+    # once and do all the recognitions you need then, deinitialize it.
+    checkResult("Process",
+                ultimateMrzSdk.UltMrzSdkEngine_process(
+                    imageType,
+                    image.tobytes(), # type(x) == bytes
+                    width,
+                    height,
+                    0, # stride
+                    1 # exifOrientation (already rotated in load_image -> use default value: 1)
+                )
+        )
+
+    # Press any key to exit
+    input("\nPress Enter to exit...\n") 
+
+    # DeInit the engine
+    checkResult("DeInit", 
+                ultimateMrzSdk.UltMrzSdkEngine_deInit()
+               )
+    
+    
---
+++ TestCases/Second Test Case/faceRecognition/gender_by_name.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+import requests, time, json
+from requests import Request
+from requests.packages.urllib3.exceptions import InsecureRequestWarning, SNIMissingWarning, InsecurePlatformWarning
+
+
+class Genderizeio(object):
+    """
+    A simple python wrapper for the Genderize.io API.
+    Written to manage rate limiting and keep track of time window resets.
+    Though this class supports the use of a proxy - please adhere to the rate limits set by Genderize.io
+    and consider supporting the developers at https://store.genderize.io/ if you have need for exceeding them.
+    """
+
+    API_URL = "https://api.genderize.io/"
+
+    def __init__(self, proxies=None):
+        """
+        Constructor establishes a requests session to be used throughout.
+        Args:
+            proxies (optional): A dictionary of proxies where the keys are the protocols.
+        """
+
+        # Disable requests library SSL warnings.
+        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+        requests.packages.urllib3.disable_warnings(SNIMissingWarning)
+        requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
+
+        self.proxies = proxies if proxies is not None else {} # Blank dictionary if no proxies provided
+
+        self.session = requests.Session()
+        self.session.proxies = self.proxies
+
+        # Response header information to manage rate limiting
+        self.last_request_time = 0
+        self.time_until_reset = 0
+        self._rate_limit = 1000
+        self._rate_limit_remaining = 0
+
+
+    def genderize(self, names):
+        """
+        Makes API call with provided names and returns a dictionary or array
+        of dictionaries with the results.
+        Args:
+             names: List or String containing names/name to query API with.
+        Return:
+            Dictionary or list of dictionaries if multiple names queried
+        """
+
+        query_names = [names] if isinstance(names, str) else names  # Convert single string input to list
+        
+        if len(query_names) > 10:
+            raise Exception('Genderize API has a 10 name limit for batch requests.')
+
+        query_params = {'name[]': [name for name in query_names]}  # Create our GET query parameters
+
+        # Build and prepare our request for easier debugging
+        request = Request('GET', self.API_URL, params=query_params)
+        prepared_request = self.session.prepare_request(request)
+
+        response = self.session.send(prepared_request)
+
+        self.last_request_time = time.time()  # Record the time we made the request for rate limiting
+
+        # Create a switch/case for our response status
+        def response_switch(x):
+            return {
+                200: {'success': True},
+                400: {'success': False, 'reason': 'Bad request'},
+                429: {'success': False, 'reason': 'Rate limit hit'},
+                500: {'success': False, 'reason': 'Internal Server Error'}
+            }[x]
+
+        response_status = response_switch(response.status_code)  # Evaluate our status against our switch/case
+
+        # Raise an exception with the respective reason should our request fail
+        if not response_status['success']:
+            raise Exception(response_status['reason'])
+
+        # Parse our response headers
+        response_headers = response.headers
+        self._rate_limit = response_headers['X-Rate-Limit-Limit']
+        self._rate_limit_remaining = response_headers['X-Rate-Limit-Remaining']
+        self.time_until_reset = int(response_headers['X-Rate-Reset'])
+
+        # Parse our response content
+        response_content = json.loads(response.text)
+
+        return response_content
+
+
+    @property
+    def rate_limit_remaining(self):
+        return self._rate_limit_remaining
+
+    @property
+    def rate_limit(self):
+        return self._rate_limit
+
+    @property
+    def rate_limit_time_remaining(self):
+
+        # Calculate the time remaining from the last request time
+        calculated_time_remaining =  self.time_until_reset - (time.time() - self.last_request_time)
+
+        # Return 0 if the time remaining is a negative value
+        return int(calculated_time_remaining) if int(calculated_time_remaining) > 0 else 0
